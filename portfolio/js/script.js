5c561c6bce72f1680ffebf36aaf659faabb8a149
kujira <woodwise03@gmail.com>
2020-05-15 00:21:47 +0900

    レスポンシブ画面に対応。

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,17 +1,26 @@
-
-    var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-container', {
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        slidesPerView: 4,
-        spaceBetween: 0,
-        initialSlide: 0.5,
+        slidesPerView: 1,
+        width: 322,
+        spaceBetween: 50,
         loop: true,
+        breakpoints: {
+          768: {
+            slidesPerView: 4,
+            width: 1100, 
+            spaceBetween: 0,
+            initialSlide: 0.5,
+            loop: true,
+          }
+        },
         autoplay: {
             delay: 5000,
             disableOnInteraction: true
         }
+        
     });
     
     $( '.js-faq' ).on( 'click', function() {
@@ -19,27 +28,49 @@
         $( this ).toggleClass( 'add-active' );
     });
 
-    $(function(){
-        $('a[href^="#"]').click(function(){
-          var speed = 500;
-          var href= $(this).attr("href");
-          var target = $(href == "#" || href == "" ? 'html' : href);
-          var position = target.offset().top;
-          $("html, body").animate({scrollTop:position}, speed, "swing");
-          return false;
-        });
-      });
-
       jQuery(function(){
         jQuery(window).scroll(function (){
             jQuery('.fade_off').each(function(){
                 var elemPos = jQuery(this).offset().top;
                 var scroll = jQuery(window).scrollTop();
                 var windowHeight = jQuery(window).height();
-                if (scroll > elemPos - windowHeight + 100){
+                if (scroll > elemPos - windowHeight + 50){
                     jQuery(this).addClass('fade_on');
                 }
             });
         });
         jQuery(window).scroll();
       });
+
+        var $header = $( '#js-header' );
+        
+        // navigation
+        $( '#js-toggle' ).on( 'click', function(e) {
+          e.preventDefault();
+          $header.toggleClass( 'add-active' );
+        });
+        
+        $( '.js-navLink, #js-overlay' ).on( 'click', function(e) {
+          e.preventDefault();
+          $header.removeClass( 'add-active' );
+        });
+    
+        var pcWidth = window.matchMedia( 'screen and (min-width: 769px)' );
+        function checkBreakPoint() {
+          if( pcWidth.matches ) {
+            $header.removeClass( 'add-active' );
+          }
+        }
+        pcWidth.addListener( checkBreakPoint );
+        checkBreakPoint();
+    
+    
+        // smooth scroll
+        $( 'a[href^="#"]' ).on( 'click', function(e){
+          var speed = 500;
+          var href= $( this ).attr( 'href' );
+          var target = $( href === '#' || href === '' ? 'html' : href );
+          var position = target.offset().top - $header.outerHeight();
+          $( 'html, body' ).animate( { scrollTop:position }, speed, 'swing' );
+          e.preventDefault();
+        });
